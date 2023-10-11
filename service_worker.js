75270346e1c0073947dae03a2571772ed4fdb983
Endzelisp <endzelisp@gmail.com>
2023-10-11 05:08:57 -0400

    Add necesary files to enable PWA capabilities

---
+++ service_worker.js
@@ -0,0 +1,63 @@
+const CACHE_NAME = 'V1_cache_recordador_3000';
+const urlsToCache = [
+  './',
+  './styles.css',
+  './index.js',
+  './img/cat-lover.png',
+  './img/icon_16.png',
+  './img/icon_24.png',
+  './img/icon_32.png',
+  './img/icon_64.png',
+  './img/icon_128.png',
+  './img/icon_256.png',
+  './img/icon_512.png',
+]
+
+//durante la fase de instalación, generalmente se almacena en caché los activos estáticos
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache)
+          .then(() => self.skipWaiting())
+      })
+      .catch(err => console.log('Falló registro de cache', err))
+  )
+})
+
+//una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
+self.addEventListener('activate', e => {
+  const cacheWhitelist = [CACHE_NAME]
+
+  e.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            //Eliminamos lo que ya no se necesita en cache
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName)
+            }
+          })
+        )
+      })
+      // Le indica al SW activar el cache actual
+      .then(() => self.clients.claim())
+  )
+})
+
+//cuando el navegador recupera una url
+self.addEventListener('fetch', e => {
+  //Responder ya sea con el objeto en caché o continuar y buscar la url real
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => {
+        if (res) {
+          //recuperar del cache
+          return res
+        }
+        //recuperar de la petición a la url
+        return fetch(e.request)
+      })
+  )
+})
